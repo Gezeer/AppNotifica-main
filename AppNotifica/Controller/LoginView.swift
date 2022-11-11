1d46849dc84921ce036f73564f2a1b7806e107f9
Geiziane Alexandre <geizealexandre@gmail.com>
2022-11-11 16:10:51 -0300

    Elementos na LoginView
    Adicionando elementos como text, button, imagem e label

--- AppNotifica/Controller/LoginView.swift
+++ AppNotifica/Controller/LoginView.swift
@@ -12,7 +12,118 @@ class LoginView:UIView{
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .white
+        setupVisualElements()
+        
     }
+    //cria a função com as propriedade da Imagem do login
+    var imageLogin: UIImageView = {
+        let imagem = UIImageView()
+        imagem.image = UIImage(named:"ImageLogin")
+        imagem.contentMode = .scaleAspectFit
+        imagem.translatesAutoresizingMaskIntoConstraints = false
+        return imagem
+    }()
+    
+    //cria a função com as propriedade do label do login
+    var labelLogin: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(red: 0.541, green: 0.541, blue: 0.557, alpha: 1)
+        label.font = UIFont(name: "SFProDisplay-Light", size: 16)
+        label.text = "Registre e gerencie as ocorrências do seu IF"
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+        textField.placeholder = "E-mail"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    var SenhaTextField: UITextField = {
+        let senhaText = UITextField()
+        senhaText.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+        senhaText.placeholder = "Senha"
+        senhaText.translatesAutoresizingMaskIntoConstraints = false
+        return senhaText
+    }()
+    
+    var button: UIButton = {
+        let buttonField = UIButton()
+        buttonField.layer.backgroundColor = UIColor(red: 0.369, green: 0.639, blue: 0.639, alpha: 1).cgColor
+        buttonField.setTitle("LOGAR", for: .normal)
+        buttonField.layer.cornerRadius = 14
+        buttonField.translatesAutoresizingMaskIntoConstraints = false
+        return buttonField
+    }()
+    var buttonTwo: UIButton = {
+        let buttonTwoField = UIButton()
+        buttonTwoField.layer.backgroundColor = UIColor(red: 0.369, green: 0.639, blue: 0.639, alpha: 1).cgColor
+        buttonTwoField.setTitle("REGISTRAR", for: .normal)
+        buttonTwoField.layer.cornerRadius = 14
+        buttonTwoField.translatesAutoresizingMaskIntoConstraints = false
+        return buttonTwoField
+    }()
+    
+    func setupVisualElements(){
+        
+        self.addSubview(imageLogin)
+        self.addSubview(labelLogin)
+        self.addSubview(emailTextField)
+        self.addSubview(SenhaTextField)
+        self.addSubview(button)
+        self.addSubview(buttonTwo)
+        
+        NSLayoutConstraint.activate([
+            imageLogin.widthAnchor.constraint(equalToConstant: 274.99),
+            imageLogin.heightAnchor.constraint(equalToConstant: 82.64),
+            imageLogin.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 57),
+            imageLogin.topAnchor.constraint(equalTo: self.topAnchor, constant: 238),
+            imageLogin.trailingAnchor.constraint(equalTo: self.trailingAnchor,constant: -57),
+            
+            
+            labelLogin.widthAnchor.constraint(equalToConstant: 100),
+            labelLogin.topAnchor.constraint(equalTo: imageLogin.topAnchor, constant: 80),
+            labelLogin.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 25),
+            labelLogin.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            
+            
+            emailTextField.widthAnchor.constraint(equalToConstant: 100),
+            emailTextField.topAnchor.constraint(equalTo: labelLogin.topAnchor, constant: 80),
+            emailTextField.heightAnchor.constraint(equalToConstant: 44),
+            emailTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 25),
+            emailTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            
+            SenhaTextField.widthAnchor.constraint(equalToConstant: 100),
+            SenhaTextField.topAnchor.constraint(equalTo: emailTextField.topAnchor, constant: 50),
+            SenhaTextField.heightAnchor.constraint(equalToConstant: 44),
+            SenhaTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 25),
+            SenhaTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            
+            button.widthAnchor.constraint(equalToConstant: 358),
+            button.heightAnchor.constraint(equalToConstant: 50),
+            button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            button.topAnchor.constraint(equalTo: SenhaTextField.topAnchor, constant: 60),
+            button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            
+            buttonTwo.widthAnchor.constraint(equalToConstant: 358),
+            buttonTwo.heightAnchor.constraint(equalToConstant: 50),
+            buttonTwo.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            buttonTwo.topAnchor.constraint(equalTo: button.topAnchor, constant: 60),
+            buttonTwo.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            
+            
+            
+            
+            
+            
+        ])
+    }
+    
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
